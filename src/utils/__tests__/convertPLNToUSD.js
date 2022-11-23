f1bd3e9a0d11de99a16680c5cb16df8c9e2dd1b7
Robert Martyniuk <r.martyniuk1234@gmail.com>
2022-11-23 12:11:29 +0100

    add new files

---
+++ src/utils/__tests__/convertPLNToUSD.js
@@ -0,0 +1,13 @@
+import { convertPLNToUSD } from './../convertPLNtoUSD';
+
+describe('ConvertPLNtoUSD', () => {
+    it('should return proper value when good input', () => {
+        expect(convertPLNToUSD(1)).toBe('$0.29');
+        expect(convertPLNToUSD(2)).toBe('$0.57');
+        expect(convertPLNToUSD(20)).toBe('$5.71');
+        expect(convertPLNToUSD(12)).toBe('$3.43');
+    });
+    it('should return NaN when input is text', () => {
+        expect(convertPLNToUSD())
+    })
+});
